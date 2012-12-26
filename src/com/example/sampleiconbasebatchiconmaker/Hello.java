6d54ba72b2cbaece28e6d4d105852f6314872027
dcsms <morondroid@gmail.com>
2012-12-26 09:17:36 -0800

    hell

---
+++ src/com/example/sampleiconbasebatchiconmaker/Hello.java
@@ -0,0 +1,14 @@
+package com.example.sampleiconbasebatchiconmaker;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Hello extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.hello);
+		
+	}
+}
